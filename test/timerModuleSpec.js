1ca7e2cd4f934a367e11ed13fd9a244dbcaa9b9f
xelita <admin@xelita.com>
2014-06-02 23:10:40 +0200

    Covering directive with unit tests.

--- test/timerModuleSpec.js
+++ test/timerModuleSpec.js
@@ -32,8 +32,6 @@ describe("angularTimerModule Tests Suite", function () {
         var angularTimerService;
         var angularTimerConstants;
 
-        var timerCallback;
-
         beforeEach(function () {
             module('angularTimerModule');
             inject(function (_angularTimerConstants_, _angularTimerService_, _$rootScope_) {
@@ -73,6 +71,115 @@ describe("angularTimerModule Tests Suite", function () {
             spyOn(window, 'clearInterval');
             angularTimerService.stopTimer('timerId');
             expect(window.clearInterval).toHaveBeenCalledWith('timerId');
+        });
+    });
+
+    // ngTimer
+
+    describe('ngTimer tests', function () {
+
+        var angularTimerService;
+        var angularTimerConstants;
+
+        var scope;
+        var element;
+
+        beforeEach(function () {
+            module('angularTimerModule');
+            inject(function (_angularTimerConstants_, _angularTimerService_, _$rootScope_) {
+                angularTimerService = _angularTimerService_;
+                angularTimerConstants = _angularTimerConstants_;
+
+                scope = _$rootScope_.$new();
+            });
+        });
+
+        it("should watch startAuto and start timer if autoStart is set to true", function () {
+            inject(function ($compile) {
+                element = $compile("<ng-timer start-auto='{{startAuto}}'></ng-timer>")(scope);
+            });
+
+            spyOn(angularTimerService, 'startTimer');
+
+            scope.startAuto = true;
+            scope.$digest();
+
+            var expectedTimerConfig = {
+                'autoStart': true,
+                'eventName': angularTimerConstants.defaultSettings.eventName,
+                'intervalMs': angularTimerConstants.defaultSettings.intervalMs
+            };
+            expect(angularTimerService.startTimer).toHaveBeenCalledWith(expectedTimerConfig);
+        });
+
+        it("should watch startAuto and not start timer if autoStart is not set to true", function () {
+            inject(function ($compile) {
+                element = $compile("<ng-timer start-auto='{{startAuto}}'></ng-timer>")(scope);
+            });
+
+            spyOn(angularTimerService, 'startTimer');
+
+            scope.startAuto = false;
+            scope.$digest();
+
+            expect(angularTimerService.startTimer).not.toHaveBeenCalled();
+        });
+
+        it("should watch eventName", function () {
+            inject(function ($compile) {
+                element = $compile("<ng-timer start-auto='true' event-name='{{eventName}}'></ng-timer>")(scope);
+            });
+
+            spyOn(angularTimerService, 'startTimer');
+
+            scope.eventName = 'custom-tick';
+            scope.$digest();
+
+            var expectedTimerConfig = {
+                'autoStart': true,
+                'eventName': 'custom-tick',
+                'intervalMs': angularTimerConstants.defaultSettings.intervalMs
+            };
+            expect(angularTimerService.startTimer).toHaveBeenCalledWith(expectedTimerConfig);
+        });
+
+        it("should watch intervalMs", function () {
+            inject(function ($compile) {
+                element = $compile("<ng-timer start-auto='true' interval-ms='{{intervalMs}}'></ng-timer>")(scope);
+            });
+
+            spyOn(angularTimerService, 'startTimer');
+
+            scope.intervalMs = 1000;
+            scope.$digest();
+
+            var expectedTimerConfig = {
+                'autoStart': true,
+                'eventName': angularTimerConstants.defaultSettings.eventName,
+                'intervalMs': 1000
+            };
+            expect(angularTimerService.startTimer).toHaveBeenCalledWith(expectedTimerConfig);
+        });
+
+        it("should stop timer if a timer is running", function () {
+            inject(function ($compile) {
+                element = $compile("<ng-timer start-auto='{{startAuto}}'></ng-timer>")(scope);
+            });
+
+            spyOn(angularTimerService, 'startTimer').and.callThrough();
+            spyOn(angularTimerService, 'stopTimer').and.callThrough();
+
+            scope.startAuto = false;
+            scope.$digest();
+            expect(angularTimerService.startTimer).not.toHaveBeenCalled();
+
+            scope.startAuto = true;
+            scope.$digest();
+            expect(angularTimerService.startTimer).toHaveBeenCalled();
+
+            scope.startAuto = false;
+            scope.$digest();
+            expect(angularTimerService.stopTimer).toHaveBeenCalled();
         });
     });
 });
